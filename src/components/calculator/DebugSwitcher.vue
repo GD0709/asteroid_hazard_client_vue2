3fb07ad157c13f98ac3667bb585e5628f3a91a5d
GD0709 <glazachevd@gmail.com>
2022-03-28 14:40:05 +0300

    Atmospheric disturbances results

---
+++ src/components/calculator/DebugSwitcher.vue
@@ -0,0 +1,21 @@
+<template>
+    
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue, Model, ModelSync } from 'vue-property-decorator';
+
+import State from '@/components/model/State';
+@Component({
+    components: {
+
+    }
+})
+export default class Blank extends Vue {
+    state: State = State.state;
+}
+</script>
+
+<style scoped lang="scss">
+
+</style>
