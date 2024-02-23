c563c7c27b9068735faed082c2b0a7d15aff1441
Glazachev Dmitry <glazachev.d@wildberries.work>
2024-02-23 13:27:45 +0300

    empty vue3 vuetify i18n

---
+++ client/src/main.ts
@@ -0,0 +1,38 @@
+/**
+ * main.ts
+ *
+ * Bootstraps Vuetify and other plugins then mounts the App`
+ */
+
+// Plugins
+import { registerPlugins } from '@/plugins'
+
+// Components
+import App from './App.vue'
+
+// Composables
+import { createApp } from 'vue'
+import { createI18n } from 'vue-i18n'
+
+const i18n = createI18n({
+  // something vue-i18n options here ...
+})
+
+
+
+import LatLon from 'geodesy/latlon-ellipsoidal-vincenty.js';
+
+const p1 = new LatLon(-37.95103, 144.42487);
+const dist = 54972.271;
+const brng = 306.86816;
+const p2 = p1.destinationPoint(dist, brng);
+
+console.log("heloo from geodesy", p2);
+
+
+
+const app = createApp(App)
+
+registerPlugins(app)
+app.use(i18n)
+app.mount('#app')
